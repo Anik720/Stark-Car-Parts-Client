d8ea2ac0892e30aba2a6246f78192e6865fb8814
Your Name <golam.anik@northsouth.edu>
2022-05-26 22:31:34 +0600

    Almost done

--- README.md
+++ README.md
@@ -1,70 +1,15 @@
-# Getting Started with Create React App
+# Stark Car Parts
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Stark Car Parts [Stark Car Parts](https://manufacturer-website-740a2.web.app/).
 
-## Available Scripts
+## Features of this site
 
-In the project directory, you can run:
+* In this website a user can login via email and password or using the google login. Once he logged into the system he will able to purchase a product and also he will able to see Dashboard and his name in the navbar
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+* For purchasing a product a user must purchase the minimumqauntity that a product has or he can't buy much then the available product.Once he purchase a product he will able to his purchase orders into the dashboard where he can see a field My orders.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+* Into the Dashboard user can see my profile, my orders,add review field where an admin can see other fields like My Profile, Manage All Orders, Add A Product, Make Admin, Manage Products. 
+* A user can also pay for the purchasing product total amount via credit card(Implemented using stripe).He can also cancel an order.Moreover he can reviews about the exprince of the website. For the admin , he can create products,delete products, can update the status of a order and will able to make other users admin.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+* There are two types of role in this website. For admin, all the routes are protected. Also for the user role, the routes are protected. A user can't see other users informations.
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,27 +2,130 @@ import React from 'react';
 
 const Blogs = () => {
   return (
-    <div>
+    <div className=''>
       <div className='grid lg:grid-cols-2 md:grid-cols-1 gap-10'>
         <div>
-          <img
-            src='https://www.xenonstack.com/hubfs/xenonstack-optimizing-react-application.png'
-            alt=''
-          />
+          <div>
+            <img
+              src='https://www.xenonstack.com/hubfs/xenonstack-optimizing-react-application.png'
+              alt=''
+            />
+          </div>
+          <div className='flex items-center'>
+            <ul>
+              <h1 className='text-xl font-bold'>
+                How will you improve the performance of a React Application?
+              </h1>
+              <li>Use React.Fragment to Avoid Adding Extra Nodes to the DOM</li>
+              <li>Use Production Build</li>
+              <li>
+                Use React.Suspense and React.Lazy for Lazy Loading Components
+              </li>
+              <li>Use React.memo for Component Memoization</li>
+              <li>Virtualize a Large List Using react-window</li>
+            </ul>
+          </div>
         </div>
-        <div className='flex items-center'>
-          <ul>
-            <h1 className='text-xl font-bold'>
-              How will you improve the performance of a React Application?
-            </h1>
-            <li>Use React.Fragment to Avoid Adding Extra Nodes to the DOM</li>
-            <li>Use Production Build</li>
-            <li>
-              Use React.Suspense and React.Lazy for Lazy Loading Components
-            </li>
-            <li>Use React.memo for Component Memoization</li>
-            <li>Virtualize a Large List Using react-window</li>
-          </ul>
+        <div>
+          <div>
+            <img
+              src='https://www.freecodecamp.org/news/content/images/2022/03/lautaro-andreani-UYsBCu9RP3Y-unsplash.jpg'
+              alt=''
+            />
+          </div>
+          <div className='flex items-center'>
+            <ul>
+              <h1 className='text-xl font-bold'>
+                What are the different ways to manage a state in a React
+                application?
+              </h1>
+              <li>Local state.</li>
+              <li>Global state.</li>
+              <li>Server state.</li>
+              <li>URL state.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img
+              src='https://www.javascripttutorial.net/wp-content/uploads/2022/01/JavaScript-prototypal-inheritance-inherits-from-Object.svg'
+              alt=''
+            />
+          </div>
+          <div className='flex items-center'>
+            <ul>
+              <h1 className='text-xl font-bold'>
+                How will you improve the performance of a React Application?
+              </h1>
+              <li>Use React.Fragment to Avoid Adding Extra Nodes to the DOM</li>
+              <li>Use Production Build</li>
+              <li>
+                Use React.Suspense and React.Lazy for Lazy Loading Components
+              </li>
+              <li>Use React.memo for Component Memoization</li>
+              <li>Virtualize a Large List Using react-window</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img
+              src='https://www.xenonstack.com/hubfs/xenonstack-optimizing-react-application.png'
+              alt=''
+            />
+          </div>
+          <div className='flex items-center'>
+            <ul>
+              <h1 className='text-xl font-bold'>
+                How will you improve the performance of a React Application?
+              </h1>
+              <li>Use React.Fragment to Avoid Adding Extra Nodes to the DOM</li>
+              <li>Use Production Build</li>
+              <li>
+                Use React.Suspense and React.Lazy for Lazy Loading Components
+              </li>
+              <li>Use React.memo for Component Memoization</li>
+              <li>Virtualize a Large List Using react-window</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img
+              src='https://www.softwaretestinghelp.com/wp-content/qa/uploads/2012/11/Unit-Testing.png'
+              alt=''
+            />
+          </div>
+          <div className='flex items-center'>
+            <div>
+              <h1 className='text-xl font-bold'>
+                {' '}
+                How will you improve the performance of a React Application?
+              </h1>
+
+              <p>
+                A unit test is a way of testing a unit - the smallest piece of
+                code that can be logically isolated in a system. In most
+                programming languages, that is a function, a subroutine, a
+                method or property. The isolated part of the definition is
+                important. In his book "Working Effectively with Legacy Code",
+                author Michael Feathers states that such tests are not unit
+                tests when they rely on external systems: “If it talks to the
+                database, it talks across the network, it touches the file
+                system, it requires system configuration, or it can't be run at
+                the same time as any other test." Modern versions of unit
+                testing can be found in frameworks like JUnit, or testing tools
+                like TestComplete. Look a little further and you will find
+                SUnit, the mother of all unit testing frameworks created by Kent
+                Beck, and a reference in chapter 5 of The Art of Software
+                Testing . Before that, it's mostly a mystery. I asked Jerry
+                Weinberg about his experiences with unit testing -- "We did unit
+                testing in 1956. As far as I knew, it was always done, as long
+                as there were computers".
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
