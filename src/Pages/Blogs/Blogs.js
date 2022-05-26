95e8cd3af49c19ad20c6776584c3b0381c43518c
Your Name <golam.anik@northsouth.edu>
2022-05-26 23:09:57 +0600

    Almost done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -56,14 +56,17 @@ const Blogs = () => {
           <div className='flex items-center'>
             <ul>
               <h1 className='text-xl font-bold'>
-                How will you improve the performance of a React Application?
+                How does prototypical inheritance work?
               </h1>
               <p>
-                The Prototypal Inheritance is a feature in javascript used to
-                add methods and properties in objects. It is a method by which
-                an object can inherit the properties and methods of another
-                object. Traditionally, in order to get and set the [[Prototype]]
-                of an object, we use Object. getPrototypeOf and Object
+                Every object with its methods and properties contains an
+                internal and hidden property known as [[Prototype]]. The
+                Prototypal Inheritance is a feature in javascript used to add
+                methods and properties in objects. It is a method by which an
+                object can inherit the properties and methods of another object.
+                Traditionally, in order to get and set the [[Prototype]] of an
+                object, we use Object.getPrototypeOf and Object.setPrototypeOf.
+                Nowadays, in modern language, it is being set using __proto__.
               </p>
             </ul>
           </div>
