b74b7a7479de81baabb84d01c387a46152aaa300
Your Name <golam.anik@northsouth.edu>
2022-05-26 23:13:27 +0600

    Almost done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -74,7 +74,7 @@ const Blogs = () => {
         <div>
           <div>
             <img
-              src='https://www.xenonstack.com/hubfs/xenonstack-optimizing-react-application.png'
+              src='https://i0.wp.com/css-tricks.com/wp-content/uploads/2018/04/setState.jpg?fit=1200%2C600&ssl=1'
               alt=''
             />
           </div>
